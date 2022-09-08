48e8b3129c32ba489ac974fbf5d12c4798fdce70
Tkistler12 <99052162+Tkistler12@users.noreply.github.com>
2022-09-07 21:52:08 -0400

    Adding this to Github

---
+++ TestMyStringLib.java
@@ -0,0 +1,25 @@
+public class TestMyStringLib { 
+   public static void main ( String [] args) { 
+   
+   
+   String str = "Hello World There"; 
+   String [] test = MyStringLib.tokenize(str); 
+   for ( String value: test) {
+      System.out.println(value); 
+   }  
+   
+   String str2 = "I am kind of nervous for this class! "; 
+   String [] test2 = MyStringLib.tokenize(str2); 
+   for ( String value: test2) {
+      System.out.println(value); 
+   }  
+
+   
+   
+   
+   } // end of main 
+
+
+
+
+}// end of class 
